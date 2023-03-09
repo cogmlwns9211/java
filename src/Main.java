ac204702634e4465e9da006d078498164fc72ac0
cogmlwns9211 <abc000408@naver.com>
2023-03-10 06:15:04 +0900

    최초저장

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=1,b=0,c=0;
+        for(a=1;a<=100;a++){
+            System.out.println(b+" + "+a+" = "+(b+=a));
+        }
+    }
+}
